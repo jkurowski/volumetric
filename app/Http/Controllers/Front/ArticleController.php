bf35822a90be733b10454dfdd76e2e5d2f71507c
Jacek <kontakt@jk-design.pl>
2021-08-25 16:04:08 +0200

    Users and Sliders

--- app/Http/Controllers/Front/ArticleController.php
+++ app/Http/Controllers/Front/ArticleController.php
@@ -28,14 +28,14 @@ class ArticleController extends Controller
             ->description($article->content_entry)
             ->datePublished($article->created_at)
             ->dateModified($article->updated_at)
-            ->image(Schema::imageObject()->url('http://developro.4dl-dev.pl/public/uploads/articles/share/'.$article->file)
+            ->image(Schema::imageObject()->url(asset('uploads/articles/'.$article->file))
                 ->height(Article::IMG_HEIGHT)
                 ->width(Article::IMG_WIDTH))
             ->author(Schema::person()->name('Autor'));
 
         $og = OpenGraph::title($article->title)
             ->type('article')
-            ->image(asset('uploads/articles/'.$article->file), [
+            ->image('http://developro.4dl-dev.pl/public/uploads/articles/share/'.$article->file, [
                 'width' => 600,
                 'height' => 314
             ])
